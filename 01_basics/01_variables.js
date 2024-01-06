cf3e4543b9b29c3bec1b7a72c8d01c292c1b6e4a
Navjyot21 <155626691+Navjyot21@users.noreply.github.com>
2024-01-06 06:36:45 +0000

    conatainer config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accontEmail =" hitesh@123gmail.com"
+var accountPassword ="12345"
+accountCity= "Jaipur "
+
+accontEmail = "hc@hc.com"
+accountPassword ="21212121"
+accountCity ="Bengaluru"
+
+console.log (accountId);
+/*
+
+*/
+
+console.table([accountId , accontEmail, accountPassword , accountCity])
